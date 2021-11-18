79c6c63cc22a1ae484ffb941738b3b3a76b0c1ac
mrgoodbytes8667 <72107446+mrgoodbytes8667@users.noreply.github.com>
2021-11-18 14:25:55 -0600

    Add format option (#6)

--- Tests/PluralizeTest.php
+++ Tests/PluralizeTest.php
@@ -3,6 +3,7 @@
 namespace Bytes\PluralizeBundle\Tests;
 
 use Bytes\PluralizeBundle\Pluralize;
+use Generator;
 use PHPUnit\Framework\TestCase;
 
 class PluralizeTest extends TestCase
@@ -12,6 +13,7 @@ class PluralizeTest extends TestCase
      * @dataProvider provideValues
      * @param $number
      * @param $string
+     * @param $expectedFull
      * @param $expected
      */
     public function testGetPluralizedString($number, $string, $expectedFull, $expected)
@@ -23,22 +25,49 @@ class PluralizeTest extends TestCase
         $this->assertEquals($expectedFull, $results);
     }
 
-    public function provideValues()
+    /**
+     * @dataProvider provideValues
+     * @dataProvider provideFormattedValues
+     * @param $number
+     * @param $string
+     * @param $expectedFull
+     * @param $expected
+     */
+    public function testGetFormattedPluralizedString($number, $string, $expectedFull, $expected)
     {
-        yield['number' => 0, 'string' => 'word', 'expectedFull' => '0 words', 'expected' => 'words'];
-        yield['number' => 1, 'string' => 'word', 'expectedFull' => '1 word', 'expected' => 'word'];
-        yield['number' => 2, 'string' => 'word', 'expectedFull' => '2 words', 'expected' => 'words'];
+        $results = Pluralize::pluralizeFormatted($number, $string);
+        $this->assertEquals($expectedFull, $results);
+    }
 
-        yield['number' => 0, 'string' => 'leaves', 'expectedFull' => '0 leafs', 'expected' => 'leafs'];
-        yield['number' => 1, 'string' => 'leaves', 'expectedFull' => '1 leaf', 'expected' => 'leaf'];
-        yield['number' => 2, 'string' => 'leaves', 'expectedFull' => '2 leafs', 'expected' => 'leafs'];
+    /**
+     * @return Generator
+     */
+    public function provideValues(): Generator
+    {
+        yield ['number' => 0, 'string' => 'word', 'expectedFull' => '0 words', 'expected' => 'words'];
+        yield ['number' => 1, 'string' => 'word', 'expectedFull' => '1 word', 'expected' => 'word'];
+        yield ['number' => 2, 'string' => 'word', 'expectedFull' => '2 words', 'expected' => 'words'];
 
-        yield['number' => 0, 'string' => 'person', 'expectedFull' => '0 persons', 'expected' => 'persons'];
-        yield['number' => 1, 'string' => 'person', 'expectedFull' => '1 person', 'expected' => 'person'];
-        yield['number' => 2, 'string' => 'person', 'expectedFull' => '2 persons', 'expected' => 'persons'];
+        yield ['number' => 0, 'string' => 'leaves', 'expectedFull' => '0 leafs', 'expected' => 'leafs'];
+        yield ['number' => 1, 'string' => 'leaves', 'expectedFull' => '1 leaf', 'expected' => 'leaf'];
+        yield ['number' => 2, 'string' => 'leaves', 'expectedFull' => '2 leafs', 'expected' => 'leafs'];
 
-        yield['number' => 0, 'string' => 'people', 'expectedFull' => '0 persons', 'expected' => 'persons'];
-        yield['number' => 1, 'string' => 'people', 'expectedFull' => '1 person', 'expected' => 'person'];
-        yield['number' => 2, 'string' => 'people', 'expectedFull' => '2 persons', 'expected' => 'persons'];
+        yield ['number' => 0, 'string' => 'person', 'expectedFull' => '0 persons', 'expected' => 'persons'];
+        yield ['number' => 1, 'string' => 'person', 'expectedFull' => '1 person', 'expected' => 'person'];
+        yield ['number' => 2, 'string' => 'person', 'expectedFull' => '2 persons', 'expected' => 'persons'];
+
+        yield ['number' => 0, 'string' => 'people', 'expectedFull' => '0 persons', 'expected' => 'persons'];
+        yield ['number' => 1, 'string' => 'people', 'expectedFull' => '1 person', 'expected' => 'person'];
+        yield ['number' => 2, 'string' => 'people', 'expectedFull' => '2 persons', 'expected' => 'persons'];
+    }
+
+    /**
+     * @return Generator
+     */
+    public function provideFormattedValues(): Generator
+    {
+        yield ['number' => 1000, 'string' => 'people', 'expectedFull' => '1,000 persons', 'expected' => 'persons'];
+        yield ['number' => 1001, 'string' => 'people', 'expectedFull' => '1,001 persons', 'expected' => 'persons'];
+        yield ['number' => 1010, 'string' => 'people', 'expectedFull' => '1,010 persons', 'expected' => 'persons'];
     }
 }
